a6c5d922be1e967a6f009e119e88757f1a04fd94
Dawid Jeż <dawidjez@gmail.com>
2021-11-11 14:37:33 +0100

    init

---
+++ src/components/Header.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    export let name: string;
+</script>
+
+<header>
+    <img src="images/camera-black.png" alt="logo">
+
+    <nav>
+        <ul>
+            <li>O mnie</li>
+            <li>O mnie</li>
+            <li>O mnie</li>
+            <li>O mnie</li>
+        </ul>
+    </nav>
+</header>
+
+<style>
+    header{
+        width: 100%;
+        height: 100px;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        color: #000;
+        position: absolute;
+        top: 0;
+    }
+</style>
